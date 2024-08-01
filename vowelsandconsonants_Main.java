31f3c9037c4d3ae6948d6f454e5b9c549d967f22
poojitha <badigantivenkatasreep@gmail.com>
2024-08-01 15:46:34 +0530

    Add files via upload

---
+++ vowelsandconsonants_Main.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class vowelsandconsonants_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		vowelsandconsonants sa = new vowelsandconsonants();
+		sa.info();
+		sa.display();
+	}
+
+}
